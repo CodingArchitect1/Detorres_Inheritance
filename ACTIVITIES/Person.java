14dec8555aa195b72bc3eb1ce713fe5aef03d987
CodingArchitect1 <detorresmori18@gmail.com>
2025-04-07 18:36:20 +0800

    Update Person.java

--- ACTIVITIES/Person.java
+++ ACTIVITIES/Person.java
@@ -1,1 +1,37 @@
 
+public class Person {
+
+    String name;
+    int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDetails() {
+        return "Name: " + name + ", ID: " + id;
+    }
+}
