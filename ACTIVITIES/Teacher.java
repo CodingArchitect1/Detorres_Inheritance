991def813a0a800387e6b780c85cf10321510d53
CodingArchitect1 <detorresmori18@gmail.com>
2025-04-07 18:37:13 +0800

    Update Teacher.java

--- ACTIVITIES/Teacher.java
+++ ACTIVITIES/Teacher.java
@@ -1,1 +1,29 @@
 
+public class Teacher extends Person {
+
+    String subject;
+
+    public Teacher(String name, int id, String subject) {
+        super(name, id);
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Subject: " + subject);
+    }
+
+    @Override
+    public String getDetails() {
+        return super.getDetails() + ", Subject: " + subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
