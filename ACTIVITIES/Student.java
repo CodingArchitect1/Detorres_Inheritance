4ec4c0b5b43883769a3ea64db12bad94c29beeb0
CodingArchitect1 <detorresmori18@gmail.com>
2025-04-07 18:36:58 +0800

    Update Student.java

--- ACTIVITIES/Student.java
+++ ACTIVITIES/Student.java
@@ -1,1 +1,29 @@
+public class Student extends Person {
 
+    String gradeLevel;
+
+    public Student(String name, int id, String gradeLevel) {
+        super(name, id);
+        this.gradeLevel = gradeLevel;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Grade Level: " + gradeLevel);
+    }
+
+    @Override
+    public String getDetails() {
+        return "Name: " + getName() + ", ID: " + getId() + ", Grade Level: " + gradeLevel;
+    }
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+
+}
